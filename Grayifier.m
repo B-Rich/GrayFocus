fca3c4e5f050409f11764f4a96850cc3d9145ebf
Dale Harvey <daleharvey@mbp.(none)>
2010-09-26 16:58:59 +0100

    make windows slightly opaque as well as grey

--- Grayifier.m
+++ Grayifier.m
@@ -16,9 +16,14 @@ extern OSStatus CGSNewConnection(const void **attributes, CGSConnection * id);
 
 static CGSWindowFilterRef grayscaleFilter;
 static CGSConnection connection;
+static NSMutableDictionary *oldWindowValues = nil;
+static double unfocusedAlphaValue = 0.6;
+static double focusedAlphaValue = 1;
 
 + (void)load
 {
+	oldWindowValues = [[NSMutableDictionary dictionary] retain];
+	
 	CGSNewConnection(NULL, &connection);
 	CGSNewCIFilterByName(connection, (CFStringRef)@"CIColorControls", &grayscaleFilter);
 	CGSSetCIFilterValuesFromDictionary(connection, grayscaleFilter, (CFDictionaryRef)[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:0] forKey:@"inputSaturation"]);
@@ -32,12 +37,32 @@ static CGSConnection connection;
 
 + (void)grayify:(NSNotification *)note
 {	
-	CGSAddWindowFilter(connection, [(NSWindow *)[note object] windowNumber], grayscaleFilter, 1 << 2);
+	NSWindow *window = (NSWindow *)[note object];
+	CGSAddWindowFilter(connection, [window windowNumber], grayscaleFilter, 1 << 2);
+	
+	NSDictionary *oldValues = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:[window isOpaque]], @"opaque",
+																		 [NSNumber numberWithDouble:[window alphaValue]], @"alphaValue", nil];
+	[oldWindowValues setObject:oldValues forKey:[NSValue valueWithPointer:window]];
+	
+	[window setOpaque:NO];
+	[window setAlphaValue:unfocusedAlphaValue];
 }
 
 + (void)colorize:(NSNotification *)note
 {
-	CGSRemoveWindowFilter(connection, [(NSWindow *)[note object] windowNumber], grayscaleFilter);
+	NSWindow *window = (NSWindow *)[note object];
+	CGSRemoveWindowFilter(connection, [window windowNumber], grayscaleFilter);
+
+	[window setOpaque:NO];
+	[window setAlphaValue:focusedAlphaValue];
+	
+  // Attempt to restore previous water values, currently broken
+	//NSDictionary *oldValues = [oldWindowValues objectForKey:[NSValue valueWithPointer:window]];
+	//if (oldValues)
+	//{
+	//	[window setOpaque:[[oldValues objectForKey:@"opaque"] boolValue]];
+	//	[window setAlphaValue:[[oldValues objectForKey:@"alphaValue"] doubleValue]];
+	//}
 }
 
 @end
